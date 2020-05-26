65b7f3053c379579431fff9d1a3f0ea3d570fcb0
Eugene Anfalov <anfalovd10@yandex.ru>
2020-05-26 17:36:06 +0500

    Create prefix.py

---
+++ prefix.py
@@ -0,0 +1,17 @@
+def prefix_function (s):
+	n = len(s)
+	pi = [0] * n
+	for i in range(1, n):
+		j = pi[i - 1]
+		while j > 0 and s[i] != s[j]:
+			j = pi[j-1]
+		if s[i] == s[j]:
+			j += 1
+		pi[i] = j
+	return pi
+
+s = input()
+t = input()
+e = s + '#' + t
+p = prefix_function(e)
+print(*p)
